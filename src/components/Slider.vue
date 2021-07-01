259724cec58d6e94b76e58c68d2602b28d6409fa
hosein sedaqat <hsedaqat1378@gmail.com>
2021-07-02 00:36:42 +0430

    swiper slider in vue

---
+++ src/components/Slider.vue
@@ -0,0 +1,39 @@
+<template>
+  <swiper
+    :slides-per-view="3"
+    :space-between="50"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    ...
+  </swiper>
+</template>
+
+<script>
+// import Swiper and SwiperSlide
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+
+// import css file for Swiper
+import "swiper/swiper-bundle.css";
+
+export default {
+  name: "Slider",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  methods: {
+    onSwiper(swiper) {
+      console.log(swiper);
+    },
+    onSlideChange() {
+      console.log("slide change");
+    },
+  },
+};
+</script>
+
+<style></style>
